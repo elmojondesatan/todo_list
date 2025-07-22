54960635b48f05cf02601225b35cc82b8fdd1b68
Diegov <davillatoro@email>
2025-07-22 08:06:55 -0600

    Se agrega la seccion en el dashboard de la carpeta vistas

---
+++ vistas/dashboard.js
@@ -0,0 +1,12 @@
+export function dashboard(){
+    
+    let seccion = document.createElement('section');
+    
+    let h1 = document.createElement('h1');
+    h1.innerText = "Hola Mundo";
+    seccion.appendChild(h1);
+
+    return seccion;
+}
+
+document.body.appendChild(dashboard());
